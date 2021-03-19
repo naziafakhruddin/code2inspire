2dcc73e3ab0e99cc94ab26797f6fdbc48ad5749c
Nazia Fakhruddin <naziafakhruddin@gmail.com>
2021-03-19 21:46:11 +0000

    Section4

---
+++ Backup/script.js
@@ -0,0 +1,29 @@
+
+
+/*-- Code for enlarging the images in the more articles section on the Main Page--*/
+    function highlight(x){
+        x.style.width="350px";
+        x.style.height="250px";
+        x.style.padding="10px";
+        x.style.boxShadow="0px 4px 8px #555";
+        
+    }
+    function normal(x){
+        x.style.width= " 300px ";
+        x.style.height=" 200px ";
+        x.style.padding= " 10px ";
+        x.style.boxShadow= " none ";
+        
+    }
+    
+   /*-- Code for confirming the information before submission in Sign Up form in Get In Touch page-- */
+    function myfunction(){
+        let firstname = document.getElementById('firstname').value;
+        let lastname = document.getElementById('lastname').value;
+        let email = document.getElementById('e-mail').value;
+        let message ='Confirm your details:\nFirst Name: '+firstname+'\n'+'Last Name: '+lastname+'\n'+'Email: '+email;
+        confirm(message);
+    }
+
+  
+   
